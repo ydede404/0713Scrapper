163408ca08954f1a84ec0d2cdb3d9b383e326f80
ydede404 <dakitse@gmail.com>
2017-07-13 16:38:23 +0200

    Avec extraction de la ligne d'adresse mais toute la ligne html est affichée

--- routedelamairie.rb
+++ routedelamairie.rb
@@ -6,5 +6,5 @@ PAGE_URL = "http://annuaire-des-mairies.com/95/vaureal.html"
 
 page = Nokogiri::HTML(open(PAGE_URL))
 emailformat = /[@]/
-get_the_email_of_a_townhal_from_its_webpage = page.css("p.Style22").select{|text| text.match emailformat}   
-get_the_email_of_a_townhal_from_its_webpage.each {|email| puts "#{email}"}   
+get_the_email_of_a_townhal_from_its_webpage = page.css("p.Style22").select{|i| i.text =~ emailformat}   
+get_the_email_of_a_townhal_from_its_webpage.each {|email| puts email}      
